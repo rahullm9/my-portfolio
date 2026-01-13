ef7de1e0931dec527c427be52c9ba57a1c6f6f94
rahullm9 <rahulmahato10101@gmail.com>
2026-01-13 20:02:10 +0530

    changes heropage

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./index.css";
 import HeroSection from "./components/HeroSection.jsx";
-import About from "./components/About.jsx";
-import Project from "./components/Project.jsx";
-import Contact from "./components/Contact.jsx";
+// import About from "./components/About.jsx";
+// import Project from "./components/Project.jsx";
+// import Contact from "./components/Contact.jsx";
 
 const App = () => {
   return (
     <div>
       <HeroSection />
-      <About />
-      <Project />
-      <Contact />
+      {/* <About /> */}
+      {/* <Project /> */}
+      {/* <Contact /> */}
     </div>
   );
 };
--- my-portfolio/src/components/HeroSection.jsx
+++ my-portfolio/src/components/HeroSection.jsx
@@ -4,53 +4,52 @@ import { FaFolderOpen } from "react-icons/fa";
 import { FaBrain } from "react-icons/fa6";
 import { IoSchoolSharp } from "react-icons/io5";
 import { IoCall } from "react-icons/io5";
-import PeakyImg from "../assets/peaky.png";
+import Particles from "../assets/Particles";
 
 const HeroSection = () => {
   const menuItems = [
-    { id: 1, label: "About", color: "bg-blue-500", icon: <FaInfoCircle /> },
-    { id: 2, label: "Project", color: "bg-green-500", icon: <FaFolderOpen /> },
-    { id: 3, label: "Skills", color: "bg-purple-500", icon: <FaBrain /> },
+    { id: 1, label: "About", color: "bg-[#535C91]", icon: <FaInfoCircle /> },
+    { id: 2, label: "Project", color: "bg-[#535C91]", icon: <FaFolderOpen /> },
+    { id: 3, label: "Skills", color: "bg-[#535C91]", icon: <FaBrain /> },
     {
       id: 4,
       label: "Education",
-      color: "bg-yellow-500",
+      color: "bg-[#535C91]",
       icon: <IoSchoolSharp />,
     },
-    { id: 5, label: "Contact", color: "bg-red-500", icon: <IoCall /> },
+    { id: 5, label: "Contact", color: "bg-[#535C91]", icon: <IoCall /> },
   ];
 
   return (
-    <div className="hero-main relative bg-[#222831] h-screen w-screen flex  justify-center gap-4 z-0 overflow-hidden">
+    <div className="hero-main relative bg-[#070F2B] h-screen w-full flex  justify-center items-center z-0 overflow-hidden">
       {/* Background Content Boxes */}
-      <div className="text-box h-full flex-1 rounded-xl p-7 text-white ">
-        <div className="bio-text flex justify-center items-center h-full">
-          <div className="bg-[#76ABAE] text-2xl font-semibold p-4 rounded-sm shadow-2xl">
-            {" "}
-            I'M A FULLSTACK DEVELOPER
-          </div>
-        </div>
-      </div>
-      <div className="image-box h-full flex-1 rounded-xl  bg-[#31363F] ">
-        <img
-          className="h-full w-full object-cover right-1 box-border"
-          src={PeakyImg}
-          alt="Peaky Blinder"
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
         />
       </div>
 
       {/* Center Text */}
-      <h1 className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl md:text-6xl lg:text-8xl font-bold text-white drop-shadow-lg z-10 whitespace-nowrap">
-        RAHUL MAHATO
-      </h1>
+      <div className="flex flex-col justify-center items-center  absolute z-1">
+        <h1 className="text-8xl font-bold  text-white ">WEB DEVELOPER</h1>
+        <h4 className="text-white">I'm a Full Stack web developer</h4>
+      </div>
 
+      {/* *****************-------sidebar----------*************** */}
       <div className="fixed right-4 top-1/2 -translate-y-1/2 flex flex-col gap-6 z-50 items-end">
         {menuItems.map((item) => (
           <div
             key={item.id}
-            className="group flex items-center justify-end w-12 h-12 bg-white rounded-full cursor-pointer overflow-hidden hover:w-40 transition-all duration-500 ease-in-out shadow-2xl border-2 border-gray-200"
+            className="group flex items-center justify-end w-12 h-12 bg-[#1B1A55] rounded-full cursor-pointer overflow-hidden hover:w-40 transition-all duration-500 ease-in-out shadow-2xl border-2 border-[#9290C3]"
           >
-            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mr-4 text-gray-800 font-bold whitespace-nowrap delay-100">
+            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 mr-4 text-white font-bold whitespace-nowrap delay-100">
               {item.label}
             </span>
 
